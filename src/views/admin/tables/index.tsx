afb97ae11b3df5a420e79727d64d3c5dc1ce6619
Darcio <dariuszcudny@gmail.com>
2024-04-05 20:53:38 +0200

    pagination

--- src/views/admin/tables/index.tsx
+++ src/views/admin/tables/index.tsx
@@ -1,15 +1,13 @@
-// import tableDataDevelopment from "./variables/tableDataDevelopment";
-// import tableDataCheck from "./variables/tableDataCheck";
-// import CheckTable from "./components/CheckTable";
-// import tableDataColumns from "./variables/tableDataColumns";
-// import tableDataComplex from "./variables/tableDataComplex";
-// import DevelopmentTable from "./components/DevelopmentTable";
-// import ColumnsTable from "./components/ColumnsTable";
-// import ComplexTable from "./components/ComplexTable";
 import { useGetRijksAPIByNameQuery } from "../../../redux/apis/RijksMuseumAPI";
 // import { useGetHarvardApiTypeQuery } from "../../../redux/apis/HarvardMuseumApi";
 import Loader from "components/loader/Loader";
-
+// import { Pagination } from "@material-ui/lab";
+import React, { useState } from "react";
+import Pagination from '@mui/material/Pagination';
+import useRijksPagination from "./components/RijksPagination";
+import Box from '@mui/material/Box';
+// import { default as rijksData } from "./components/test.json";
+// import Stack from '@mui/material/Stack';
 // import { Select, Option } from '@mui/joy';
 import RijksCard from "./components/RijksCard";
 import MultipleSelectChip from "./components/SelectMulti";
@@ -24,9 +22,19 @@ interface RijksProps  {
 
 const Tables = () => { 
   const {data, isLoading} = useGetRijksAPIByNameQuery(``)
-
   const rijksApi = undefined ?? data
-  console.log(rijksApi?.artObjects)
+  // console.log(rijksApi)
+
+  let [page, setPage] = useState(1);
+  const PER_PAGE = 4;
+  const _DATA = useRijksPagination(rijksApi?.artObjects, PER_PAGE);
+  const count = Math.ceil(rijksApi?.artObjects.length / PER_PAGE);
+
+  const handleChange = (e: any, p: React.SetStateAction<number>) => {
+    setPage(p);
+    _DATA.jump(p);
+  };
+  // console.log(rijksData)
   //  console.log(useGetHarvardApiTypeQuery(`gallery`))
 
 if(isLoading)return <Loader/>
@@ -40,17 +48,48 @@ if(isLoading)return <Loader/>
                 <MultipleSelectChip selByName={rijksApi.facets} />
               </div>          
           </Container>
+
+          <Box p="5">
+            <Pagination
+              count={count}
+              size="large"
+              page={page}
+              variant="outlined"
+              shape="rounded"
+              onChange={handleChange}
+            />
+
+          
+
+            <div className="mt-1 grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 ">
+                {_DATA.currentData().map(({links, longTitle, productionPlaces, principalOrFirstMaker, webImage}:RijksProps)=> (
+                  <RijksCard key={longTitle} link={links} title={longTitle} place={productionPlaces} web={webImage} maker={principalOrFirstMaker} />
+                ) )}        
+              </div>
+
+              
+
+            <Pagination
+              count={count}
+              size="large"
+              page={page}
+              variant="outlined"
+              shape="rounded"
+              onChange={handleChange}
+            />
+          </Box>
         
      
-      <div className="mt-5 h-full">
-        {rijksApi?.artObjects.map(({links, longTitle, productionPlaces, principalOrFirstMaker, webImage}:RijksProps)=> (
-           <RijksCard key={longTitle} link={links} title={longTitle} place={productionPlaces} web={webImage} maker={principalOrFirstMaker} />
-        ) )}
-       
+        {/* <div className="mt-1 grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 ">
+          {rijksApi?.artObjects.map(({links, longTitle, productionPlaces, principalOrFirstMaker, webImage}:RijksProps)=> (
+            <RijksCard key={longTitle} link={links} title={longTitle} place={productionPlaces} web={webImage} maker={principalOrFirstMaker} />
+          ) )}        
+        </div> */}
+  
+            {/* <Stack spacing={2}>              
+                <Pagination count={6} color="primary" />                
+            </Stack> */}
       </div>
-
-      {/* <div className="mt-5 grid h-full grid-cols-1 gap-5 md:grid-cols-2"></div> */}
-    </div>
     </main>
     
   );
